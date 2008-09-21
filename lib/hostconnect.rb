b51335580da3df4e75618e63e53ab0b73b9845ef
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2008-09-22 01:16:07 +0200

    bugfix + version boost

--- lib/hostconnect.rb
+++ lib/hostconnect.rb
@@ -23,7 +23,7 @@ module HostConnect
   module Version
     MAJOR = '0'
     MINOR = '1'
-    REVISION = '4'
+    REVISION = '5'
     def self.combined
       [MAJOR, MINOR, REVISION].join('.')
     end
