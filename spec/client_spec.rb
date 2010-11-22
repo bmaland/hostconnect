cd8ae20277b8c60d41a4348c6f15e1ea1c6e6bc9
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-11-22 15:39:31 +0100

    Fixed RSpec 2 deprecation warning in client_spec

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -4,17 +4,17 @@ describe HostConnect::Client do
   it "should have a http connection" do
     Client.http_connection.should be_an_instance_of(Net::HTTP)
   end
-  
+
   it "should post xml requests" do
     request = File.read("./spec/fixtures/requests/ping.xml")
     response = File.read("./spec/fixtures/responses/ping.xml")
     config = HostConnect.config
-    
+
     Net::HTTP::Post.should_receive(:new).with(config.path).and_return({})
-    mock = mock("response", :null_object => true)
+    mock = mock("response").as_null_object
     mock.stub!(:body).and_return(response)
     Client.http_connection.should_receive(:request).and_return(mock)
-    
+
     Client.post_xml_request(request).should == response
   end
 end
